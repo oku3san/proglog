93e417da5bdd69621428075c71eec3764828decd
oku3san <takaya0303@gmail.com>
2022-08-21 18:04:27 +0900

    Fix typo

--- internal/server/log.go
+++ internal/server/log.go
@@ -22,7 +22,7 @@ func (c *Log) Append(record Record) (uint64, error) {
   return record.Offset, nil
 }
 
-func (c *Log) Road(offset uint64) (Record, error) {
+func (c *Log) Read(offset uint64) (Record, error) {
   c.mu.Unlock()
   if offset >= uint64(len(c.records)) {
     return Record{}, ErrOffsetNotFound
