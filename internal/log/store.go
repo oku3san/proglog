b797f70d0d9de7c9b2dc59285a460ffccda71bfe
oku3san <takaya0303@gmail.com>
2022-08-25 00:04:21 +0900

    Add Test

--- internal/log/store.go
+++ internal/log/store.go
@@ -58,7 +58,7 @@ func (s *store) Read(pos uint64) ([]byte, error) {
     return nil, err
   }
   size := make([]byte, lenWidth)
-  if _, err := s.File.ReadAt(size, int64(pos+lenWidth)); err != nil {
+  if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
     return nil, err
   }
   b := make([]byte, enc.Uint64(size))
