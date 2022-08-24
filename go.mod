d577ff58faf05ca86f81a89ca5c3a2ce94ff0bf2
oku3san <takaya0303@gmail.com>
2022-08-24 23:05:46 +0900

    Add Test

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/oku3san/proglog
 go 1.18
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.8.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/log/store_test.go
@@ -0,0 +1,41 @@
+package log
+
+import (
+  "os"
+  "testing"
+
+  "github.com/stretchr/testify/require"
+)
+
+var (
+  write = []byte("hello world")
+  width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+  f, err := os.CreateTemp("", "store_append_read_test")
+  require.NoError(t, err)
+  defer os.Remove(f.Name())
+
+  s, err := newStore(f)
+  require.NoError(t, err)
+
+  testAppend(t, s)
+  testRead(t, s)
+  testReadAt(t, s)
+
+  s, err = newStore(f)
+  require.NoError(t, err)
+  testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+  t.Helper()
+  var pos uint64
+  for i := uint64(1); i < 4; i++ {
+    read, err := s.Read(pos)
+    require.NoError(t, err)
+    require.Equal(t, write, read)
+    pos += width
+  }
+}
