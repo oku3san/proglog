f43b8117d3a5035fa6201b32bedbc9f423a0411e
oku3san <takaya0303@gmail.com>
2022-08-26 22:24:58 +0900

    newIndex

--- internal/log/index.go
+++ internal/log/index.go
@@ -16,3 +16,27 @@ type index struct {
   mmap gommap.MMap
   size uint64
 }
+
+func newIndex(f *os.File, c Config) (*index, error) {
+  idx := &index{
+    file: f,
+  }
+  fi, err := os.Stat(f.Name())
+  if err != nil {
+    return nil, err
+  }
+  idx.size = uint64(fi.Size())
+  if err = os.Truncate(
+    f.Name(), int64(c.Segment.MaxIndexBytes),
+  ); err != nil {
+    return nil, err
+  }
+  if idx.mmap, err = gommap.Map(
+    idx.file.Fd(),
+    gommap.PROT_READ|gommap.PROT_WRITE,
+    gommap.MAP_SHARED,
+  ); err != nil {
+    return nil, err
+  }
+  return idx, nil
+}
