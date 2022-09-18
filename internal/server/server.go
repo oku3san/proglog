a50eded1993e8a91b4f0c1d6e4dbda9180f86ba1
oku3san <takaya0303@gmail.com>
2022-09-18 18:11:08 +0900

    Add funcs

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,9 @@
 package server
 
-import api "github.com/oku3san/proglog/api/v1"
+import (
+  "context"
+  api "github.com/oku3san/proglog/api/v1"
+)
 
 type Config struct {
   CommitLog CommitLog
@@ -18,4 +21,22 @@ func newgrpcServer(config *Config) (sev *grpcServer, err error) {
     Config: config,
   }
   return sev, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
+  *api.ProduceResponse, error) {
+  offset, err := s.CommitLog.Append(req.Record)
+  if err != nil {
+    return nil, err
+  }
+  return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
+  *api.ConsumeResponse, error) {
+  record, err := s.CommitLog.Read(req.Offset)
+  if nil != nil {
+    return nil, err
+  }
+  return &api.ConsumeResponse{Record: record}, nil
 }
