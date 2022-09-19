a867cbb2d4ff6974b79766fde7976f4a07360eac
oku3san <takaya0303@gmail.com>
2022-09-19 17:55:29 +0900

    Add func

--- internal/server/server.go
+++ internal/server/server.go
@@ -58,3 +58,26 @@ func (s *grpcServer) ProduceStream(
     }
   }
 }
+
+func (s *grpcServer) ConsumeStream(
+  req *api.ConsumeRequest,
+  stream api.Log_ConsumeStreamServer,
+) error {
+  select {
+  case <-stream.Context().Done():
+    return nil
+  default:
+    res, err := s.Consume(stream.Context(), req)
+    switch err.(type) {
+    case nil:
+    case api.ErrOffcetOutOfRange:
+      continue
+    default:
+      return err
+    }
+    if err = stream.Send(res); err != nil {
+      return err
+    }
+    req.Offset++
+  }
+}
