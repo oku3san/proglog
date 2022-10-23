a8b2a0c0e1aaa6b13d33800630c353ee0a2ed971
oku3san <takaya0303@gmail.com>
2022-10-23 23:39:57 +0900

    p.163

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -276,3 +276,45 @@ func newLogStore(dir string, c Config) (*logStore, error) {
   }
   return &logStore{log}, nil
 }
+
+func (l *logStore) FirstIndex() (uint64, error) {
+  return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+  off, err := l.HighestOffset()
+  return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+  in, err := l.Read(index)
+  if err != nil {
+    return err
+  }
+  out.Data = in.Value
+  out.Index = in.Offset
+  out.Type = raft.LogType(in.Type)
+  out.Term = in.Term
+  return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+  return l.StoreLogs([]*raft.Log{record})
+}
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+  for _, record := range records {
+    if _, err := l.Append(&api.Record{
+      Value: record.Data,
+      Term: record.Term,
+      Type: uint32(record.Type),
+    }); err != nil {
+      return err
+    }
+  }
+  return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+  return l.Truncate(max)
+}
+
